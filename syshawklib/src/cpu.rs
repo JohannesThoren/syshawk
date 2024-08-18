f5141be88954402bff7a9c7ff69ed376dc7c5cfa
Johannes Thorén <Johannes@lgjt.xyz>
2024-08-18 19:03:06 +0200

    calculates the avg cpu freq

--- syshawklib/src/cpu.rs
+++ syshawklib/src/cpu.rs
@@ -1,8 +1,8 @@
 use std::{thread, time::Duration};
 
 use serde::{Deserialize, Serialize};
-use sysinfo::{CpuExt, SystemExt};
 use sysinfo;
+use sysinfo::{CpuExt, System, SystemExt};
 
 #[derive(Debug, Clone, Serialize, Deserialize)]
 pub struct Cpu {
@@ -16,49 +16,50 @@ pub struct Cpu {
 
 impl Cpu {
     pub fn collect(sys: &mut sysinfo::System) -> Option<Cpu> {
-        // Initial refresh to initialize the CPU usage data.
         sys.refresh_cpu();
 
-        // Sleep for a short duration to allow the system to gather usage data.
-        thread::sleep(sysinfo::System::MINIMUM_CPU_UPDATE_INTERVAL);
+        thread::sleep(Duration::from_millis(100));
 
-        // Refresh again to get accurate CPU usage data.
         sys.refresh_cpu();
 
-        // Get the list of CPUs.
         let cpus = sys.cpus();
 
-        // If there are no CPUs, return None.
         if cpus.is_empty() {
             return None;
         }
 
-        // Get the brand name of the CPU. Assuming all CPUs have the same brand.
         let name = cpus[0].brand().to_string();
 
-        // Total cores (logical).
         let cores = cpus.len();
 
-        // Physical cores, if available. Fall back to the number of logical cores if not available.
         let physical_cores = sys.physical_core_count().unwrap_or(cores);
 
-        // Calculate the overall CPU usage by averaging the usage across all cores.
         let usage = cpus.iter().map(|cpu| cpu.cpu_usage()).sum::<f32>() / cores as f32;
 
-        // Collect the CPU usage per core.
         let per_core_usage: Vec<f32> = cpus.iter().map(|cpu| cpu.cpu_usage()).collect();
 
-        // Get the frequency of the first CPU (in MHz).
-        let speed = cpus[0].frequency() as f32;
+        let total_frequency: u64 = cpus.iter().map(|processor| processor.frequency()).sum();
 
-        // Return the CPU struct with the collected information.
+        // Calculate the number of processors
+        let num_processors = cpus.len() as u64;
+
+        // Compute the average frequency
+        let average_frequency = if num_processors > 0 {
+            total_frequency / num_processors
+        } else {
+            0
+        };
+
+        // Convert average frequency from Hz to MHz
+        let average_frequency_mhz = average_frequency / 1_000_000;
+
         Some(Cpu {
             name,
             cores,
             physical_cores,
             usage,
             per_core_usage,
-            speed,
+            speed: average_frequency as f32,
         })
     }
 }
